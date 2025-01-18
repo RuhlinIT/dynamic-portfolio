c62785ef5cddc0b1dc297c34eed3194895873f5f
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2025-01-18 15:13:31 -0500

    feat: reimplement navigation to use TabLayout for navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import TabLayout from './layouts/TabLayout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Testimonials from './pages/Testimonials';
@@ -9,12 +9,14 @@ import Login from './pages/Login';
 const App = () => {
     return (
         <Router>
-            <Navbar />
+            
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/testimonials" element={<Testimonials />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<TabLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="projects" element={<Projects />} />
+                    <Route path="testimonials" element={<Testimonials />} />
+                    <Route path="login" element={<Login />} />
+                </Route>
             </Routes>
         </Router>
     );
